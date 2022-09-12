bf0a406a9246385a92cdc2ac3811cc92327e6cf4
filipexcode <100590351+filipexcode@users.noreply.github.com>
2022-09-11 22:15:32 -0300

    Create 1002.c

---
+++ Linguagem_C/URI_beecrowd/1002.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double raio,area;
+    const double pi = 3.14159;
+    
+    scanf("%lf",&raio);
+    
+    area = (raio * raio) * pi;
+    
+    printf("A=%.4lf\n",area);
+    
+    return 0;
+}
