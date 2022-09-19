e3e14a0c5b62c1e429e6205b4bf5cea57c4e0f6f
filipexcode <100590351+filipexcode@users.noreply.github.com>
2022-09-19 00:36:03 -0300

    Create gerador_de_matricula.py

---
+++ Python/gerador_de_matricula.py
@@ -0,0 +1,12 @@
+# Código feito para a aula de Projeto Interdisciplinar 1, onde fiz um algoritmo que a partir de um padrão, cria matriculas.
+
+# Para utilizar, é necessário colocar as iniciais do nome em letras maiusculas, e a data de nascimento de forma simples, segue exemplo : FMS. DDMMAA 151103.
+
+nome = input("Coloque aqui as iniciais do nome")
+date = input("Coloque aqui a data de nascimento ou somente ano")
+
+print("#",nome,date)
+
+# Matrícula pronta ficaria #FMS151103.
+
+
