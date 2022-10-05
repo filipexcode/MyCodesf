f07737d532422437a4225f306ecd1dfe83f4f238
filipexcode <100590351+filipexcode@users.noreply.github.com>
2022-10-05 10:00:42 -0300

    Create calculadora_formativa.py

---
+++ Python/calculadora_formativa.py
@@ -0,0 +1,8 @@
+p1 = float(input("Nota P1: "))
+p2 = float(input("Nota P2: "))
+id = float(input("Nota ID: "))
+pic = float(input("Nota PIC: "))
+formativa = float(input("Nota Formativa: "))
+media = (p1+p2++id+pic+formativa)/3
+
+print(media)
