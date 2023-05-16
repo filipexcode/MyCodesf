3bbad399cc76ac68261500d141ff41991eb877fb
Filipe Moura <100590351+filipexcode@users.noreply.github.com>
2023-05-16 10:38:29 -0300

    Create 1134.c

---
+++ Linguagem_C/URI_beecrowd/1134.c
@@ -0,0 +1,34 @@
+//Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. 
+//Escreva um algoritmo para ler o tipo de combustível abastecido 
+//(codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel 4.Fim). 
+// Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até que seja válido). 
+//O programa será encerrado quando o código informado for o número 4.
+
+#include <stdio.h>
+ 
+int main() {
+ 
+  int x = 0;
+  int gasolina = 0;
+  int alcool = 0;
+  int diesel = 0;
+  
+  while(x != 4){
+      scanf("%d",&x);
+      
+      if(x == 1){
+          alcool += 1;
+      }else if(x == 2){
+          gasolina +=1;
+      }else if(x == 3){
+          diesel += 1;
+      }
+  }
+  
+  printf("MUITO OBRIGADO\n");
+  printf("Alcool: %d\n",alcool);
+  printf("Gasolina: %d\n",gasolina);
+  printf("Diesel: %d\n",diesel);
+  
+    return 0;
+}
